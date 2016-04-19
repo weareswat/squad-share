c1f1bd91a03f7f60007405c2bbc0e8b3ea206ae4
pawel-krysiak <pawel.krysiak@rupeal.com>
2016-04-19 18:12:28 +0100

    Fixed tests for the new list interactor

--- test/clj/squad_share/test/interactors/list_all_links.clj
+++ test/clj/squad_share/test/interactors/list_all_links.clj
@@ -3,14 +3,19 @@
             [clojure.core.async :refer [go <!!]]
             [postgres.async :as pg]
             [squad-share.config :as config]
+            [squad-share.test.test-utils :as test-utils]
             [squad-share.interactors.list-all-links :as list-all-links]))
 
-(deftest list-all-links
-  ((<!! (pg/insert! config/db {:table "squadshare.links"} {:title (str (java.util.UUID/randomUUID)), :description "Description", :url "http://test"}))
-   (let [ context {}
-          result (list-all-links/run! context)]
-        (prn result)
+(use-fixtures :each test-utils/cleanup)
 
+(deftest list-all-links
+   (let [ 
+          _ (<!! (pg/insert! config/db {:table "squadshare.links"} {:title (str (java.util.UUID/randomUUID)), :description "Description", :url "http://test"}))
+          _ (<!! (pg/insert! config/db {:table "squadshare.links"} {:title (str (java.util.UUID/randomUUID)), :description "Description", :url "http://test"}))
+          context {}
+          result (list-all-links/run! context)]
     (testing "the link is retreived by the interactor"
-      (let [result (<!! (pg/execute! config/db ["select count(title) from squadshare.links"]))]
-        (= result 1))))))
+      (let [result (<!! (pg/execute! config/db ["select * from squadshare.links"]))]
+        ( is (= (count (-> result
+                           :rows
+                           )) 2 ))))))

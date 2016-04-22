d66b56e7829854f54d1befe9e2dd94c3a865edd7
Paulo Silva <paulosilva86@gmail.com>
2016-04-22 14:41:15 +0100

    Trim link title and description.
    Why:
    
    * Because it's a small area and everything can't be visible.
    
    This change addresses the need by:
    
    * Adding (...) at the end of long titles and descriptions with a javascript function.

---
+++ resources/public/js/trim.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  
+  function trim(klass, size) {
+    $.each($(klass), function() {
+      if (this.innerHTML.length >= size) {
+        this.innerHTML = this.innerHTML.substring(0, size) +  "...";
+      }
+    });
+  }
+
+  trim(".link-title", 25);
+  trim(".description", 60);
+});
